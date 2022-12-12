34e09c6b214ef3f0d8409230b376631e8b6b783b
adip-45 <adityaprakash5642@gmail.com>
2022-12-11 23:54:42 -0500

    Final commit

---
+++ AEDFinalProject/src/service/IPropertyService.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package service;
+
+import bean.PropertyEnlist;
+
+/**
+ *
+ * @author Aditya Prakash
+ */
+public interface IPropertyService {
+    public void AddProperty(PropertyEnlist pe);
+}
